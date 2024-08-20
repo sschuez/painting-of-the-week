08bdeed6abcfafebbb656c3fb085732c200710ee
Stephen <stephens@hey.com>
2024-08-20 13:40:50 +0200

    New model for texts

--- app/services/openai_completion.rb
+++ app/services/openai_completion.rb
@@ -1,7 +1,7 @@
 class OpenaiCompletion < ApplicationService
   def initialize(params)
     @prompt_text        = params[:prompt_text]
-    @url                = "https://api.openai.com/v1/chat/completions"
+    @url                = 'https://api.openai.com/v1/chat/completions'
   end
 
   def call
@@ -18,8 +18,8 @@ class OpenaiCompletion < ApplicationService
 
   def headers
     {
-      "Content-Type": "application/json",
-      "Authorization": ["Bearer", Rails.application.credentials.dig(:open_ai, :api_key)].join(" ")
+      "Content-Type": 'application/json',
+      "Authorization": ['Bearer', Rails.application.credentials.dig(:open_ai, :api_key)].join(' ')
     }
   end
 
@@ -27,37 +27,37 @@ class OpenaiCompletion < ApplicationService
     {
       model: gpt_model,
       messages: [
-        { "role": "system", "content": "You are Bob Ross and your are the art teacher of the user. Your are passionate about painting and art and you want to encourage your students to be creative and always practising." },
-        { "role": "user", "content": @prompt_text }
+        { "role": 'system',
+          "content": 'You are Bob Ross and your are the art teacher of the user. Your are passionate about painting and art and you want to encourage your students to be creative and always practising.' },
+        { "role": 'user', "content": @prompt_text }
       ],
-      max_tokens: max_tokens,
-      temperature: temperature
+      max_tokens:,
+      temperature:
     }
   end
 
   def handle_error(error)
     puts "🔥 OpenAI: #{error.error_message}"
-    
+
     OpenStruct.new({
-      success?: false, 
-      error: error,
-      error_message: error.error_message
-    })
+                     success?: false,
+                     error:,
+                     error_message: error.error_message
+                   })
   end
 
-  def handle_success(result)        
+  def handle_success(result)
     puts "🔥 OpenAI Input: #{@prompt_text} | Response: #{get_result_hash(result)[:choices].first[1]}"
 
     OpenStruct.new({
-      success?: true, 
-      payload: result, 
-      result_hash: get_result_hash(result)
-      })
+                     success?: true,
+                     payload: result,
+                     result_hash: get_result_hash(result)
+                   })
   end
 
   def gpt_model
-    # "gpt-3.5-turbo"
-    "gpt-4"
+    'gpt-4o'
   end
 
   def max_tokens
@@ -68,13 +68,12 @@ class OpenaiCompletion < ApplicationService
     0.9
   end
 
-
   def get_result_hash(result)
     {
-      id: result["id"],
-      object: result["object"],
-      created: result["created"],
-      model: result["model"],
+      id: result['id'],
+      object: result['object'],
+      created: result['created'],
+      model: result['model'],
       choices: get_choices(result),
       usage: get_usage(result)
     }
@@ -82,11 +81,12 @@ class OpenaiCompletion < ApplicationService
 
   def get_choices(result)
     choices = {}
-    result["choices"].first.each { |k, v| choices[k.to_sym] = v }
+    result['choices'].first.each { |k, v| choices[k.to_sym] = v }
   end
 
   def get_usage(result)
     usage = {}
-    result["usage"].each { |k, v| usage[k.to_sym] = v }
+    result['usage'].each { |k, v| usage[k.to_sym] = v }
   end
 end
+
