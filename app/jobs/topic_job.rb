74fee4285f1475d5993e3d3c80327407a7597570
Stephen <stephens@hey.com>
2024-08-20 13:53:08 +0200

    change

--- app/jobs/topic_job.rb
+++ app/jobs/topic_job.rb
@@ -2,14 +2,14 @@ class TopicJob < ApplicationJob
   queue_as :default
 
   def perform(previous_topics)
-    topic = TopicCreator.call({ previous_topics: previous_topics })
+    topic = TopicCreator.call({ previous_topics: })
 
     if topic.success?
-      @topic = Topic.create_from_topic_creator(topic.payload[:choices]["message"]["content"])
+      @topic = Topic.create_from_topic_creator(topic.payload[:choices]['message']['content'])
       puts "🔥 Created topic: #{@topic.body}"
     else
       logger.error topic.error_message
-      puts "🔥 Error creating topic: #{topic.error_message}"
+      puts "🔥 Error ccreating topic: #{topic.error_message}"
     end
   end
 end
